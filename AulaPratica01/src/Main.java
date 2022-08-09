9b942967a421db551775d57b35b2cc6b789d7c87
Denilson Batista <denilsonbatista8@gmail.com>
2022-08-09 13:48:24 -0300

    git@github.com:DenilsonBSouza/Graph-Algorithms.git

---
+++ AulaPratica01/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Graph g1 = new Graph(4);
+
+        g1.addEdge(0, 1, 3);
+        g1.addEdge(1, 0, 3);
+        g1.addEdge(0, 3, 4);
+        g1.addEdge(3, 0, 4);
+
+        System.out.println(g1);
+
+        System.out.println(g1.degree(0));
+        System.out.println(g1.degree(1));
+        System.out.println(g1.degree(2));
+        System.out.println(g1.degree(3));
+    }
+}
